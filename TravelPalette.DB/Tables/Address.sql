9c0d5ce029d0bd05cb659b2d51d537f646f36ce9
Logan Vang <500201348@fvtc.edu>
2024-02-13 21:39:51 +0000

    Deleted Address.sql

--- TravelPalette.DB/Tables/Address.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE [dbo].[Address]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [StreetName] VARCHAR(255) NOT NULL,
-    [City] VARCHAR(100) NOT NULL,
-    [ZIP] VARCHAR(10) NOT NULL,
-    [State] VARCHAR(2) NOT NULL
-)
