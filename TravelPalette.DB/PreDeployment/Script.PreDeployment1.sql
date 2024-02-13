e467d934c38826484b042876625817ad6ac7bb46
Logan Vang <500201348@fvtc.edu>
2024-02-13 15:45:29 -0600

    added Tables

---
+++ TravelPalette.DB/PreDeployment/Script.PreDeployment1.sql
@@ -0,0 +1,15 @@
+﻿/*
+ Pre-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be executed before the build script.	
+ Use SQLCMD syntax to include a file in the pre-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the pre-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+drop table if exists tblListItem
+drop table if exists tblLocationTag
+drop table if exists tblSchedule
+drop table if exists tblTag
