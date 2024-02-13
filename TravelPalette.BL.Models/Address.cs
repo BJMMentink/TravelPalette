772be55401ddc0552b49be904a5d75ca0133edd2
Sam Jindra <leumas1231@gmail.com>
2024-02-13 15:03:54 -0600

    Added Address

---
+++ TravelPalette.BL.Models/Address.cs
@@ -0,0 +1,16 @@
+﻿namespace TravelPalette.BL.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+
+
+        public string StreetName { get; set; }
+
+        public string City { get; set; }
+
+        public string ZIP { get; set; }
+
+        public string State { get; set; }
+    }
+}
