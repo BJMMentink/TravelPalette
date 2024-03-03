0bd8be60ac3dfe5439fa335a6b617c278bd7d88f
Sam Jindra <leumas1231@gmail.com>
2024-03-03 10:23:01 -0600

    .

--- TravelPalette.UI/Controllers/HomeController.cs
+++ TravelPalette.UI/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace TravelPalette.UI.Controllers
             return View();
         }
 
+        public IActionResult Test()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
