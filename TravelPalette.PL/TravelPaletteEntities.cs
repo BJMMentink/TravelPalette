af24cf92a1e8330e7ede4820d94d48275e8cc4eb
Benjamin Mentink <bigben011901@gmail.com>
2024-04-25 16:06:36 -0500

    Fixed Server Connection Error

--- TravelPalette.PL/TravelPaletteEntities.cs
+++ TravelPalette.PL/TravelPaletteEntities.cs
@@ -33,7 +33,7 @@ public partial class TravelPaletteEntities : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("server-31590-300079207.database.windows.net;Database=bigprojectdb;User ID=300079207db;Password=Test123!");
+        => optionsBuilder.UseSqlServer("Server=server-31590-300079207.database.windows.net;Database=bigprojectdb;User ID=300079207db;Password=Test123!");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
